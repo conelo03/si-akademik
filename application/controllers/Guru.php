5b3fa03119e3f60dcf8a0c15f59e4e9578567594
inwananwar99 <inwan.solihudin@iconpln.co.id>
2023-04-15 15:36:02 +0700

    update controller

--- application/controllers/Guru.php
+++ application/controllers/Guru.php
@@ -1,7 +1,7 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-class Rapor extends CI_Controller {
+class Guru extends CI_Controller {
     public function absenSiswa(){
         $data = [
             'title' => 'Absensi Siswa',
