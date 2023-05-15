ea17a3186a8c74bb9788f7eafe9927094d522829
inwananwar99 <inwan.solihudin@iconpln.co.id>
2023-05-15 07:09:55 +0700

    update #6

--- application/views/akademik/profil.php
+++ application/views/akademik/profil.php
@@ -27,16 +27,16 @@
               <?php foreach ($profil as $pr) :?>
                   <div class="row text-center">
                     <div class="col-md-6">
-                      <label for="">Sejarah</label>
-                      <h5><?= $pr['sejarah'];?></h5>                      
+                      <h4 for="">Sejarah</h4>
+                      <p><?= $pr['sejarah'];?></p>                      
                     </div>
                     <div class="col-md-6">
-                      <label for="">Visi dan Misi</label>
-                      <h5><?= $pr['visi_misi'];?></h5>
+                      <h4 for="">Visi dan Misi</h4>
+                      <p><?= $pr['visi_misi'];?></p>
                     </div>
                   </div>
                   <div class="row justify-content-center">
-                    <label for="">Struktur Organisasi</label>
+                    <h4 for="">Struktur Organisasi</h4>
                     <img src="<?= base_url('assets/img/organisasi/'.$pr['struktur_organisasi']);?>" width="2200" height="1200" alt="">
                   </div>
               <?php endforeach; ?>
